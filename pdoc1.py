9bfd072a70d8fc45e03a10b51bc1b77fe8154a99
S.R. Scott <srandallscott@gmail.com>
2021-03-17 13:34:48 -0400

    ...

--- pdoc1.py
+++ pdoc1.py
@@ -5,7 +5,10 @@ import time
 from docxtpl import DocxTemplate
 
 doc = DocxTemplate("template.docx") # in same directory
-context = { 'name' : "Doctor Manhattan",'title' : "manufactured god",'company_name' : "Veidt Industries" }
+context =
+    {'name' : "Doctor Manhattan",'title' : "manufactured god",'company_name' : "Mars Corporation"}
+    {'name' : "Adrian Veidt",'title' : "self-perfected man",'company_name' : "Veidt Industries"}
+    {'name' : "Rhorschach",'title' : "confirmed loser",'company_name' : "the streets, uninc."}
 doc.render(context)
 doc.save("generated_doc.docx")
 
