860f53b09b137cb59c09cbf2ab00227f31a38318
S.R. Scott <srandallscott@gmail.com>
2021-03-17 13:31:37 -0400

    ...

--- pdoc1.py
+++ pdoc1.py
@@ -4,7 +4,7 @@ import time
 
 from docxtpl import DocxTemplate
 
-doc = DocxTemplate("my_word_template.docx")
+doc = DocxTemplate("template.docx") # in same directory
 context = { 'name' : "Doctor Manhattan",'title' : "manufactured god",'company_name' : "Veidt Industries" }
 doc.render(context)
 doc.save("generated_doc.docx")
